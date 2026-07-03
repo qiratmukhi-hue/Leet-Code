5870913c15711075e8d8e9c1e33f7cfee1e91152
Qirat Mukhi <qiratmukhi@gmail.com>
2026-07-03 17:16:57 +0530

    Time: 30 ms (95.34%), Space: 27.8 MB (86.36%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,40 @@
+class Trie(object):
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node['*'] = True
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return '*' in node
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
